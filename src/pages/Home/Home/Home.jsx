9b6213bf6552d77db15d7c715c64fa76f0cca7cb
mrana007 <rana.es.bc@gmail.com>
2024-01-19 11:21:29 +0600

    create git branch named develope

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,11 @@
 
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is Home</h2>
-        </div>
-    );
+  return (
+      <div>
+        
+      </div>
+  );
 };
 
 export default Home;
